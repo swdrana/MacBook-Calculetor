4fa94b02e087831e6a16a8ae67dd38eada15947a
swdrana <tctr2s@gmail.com>
2022-02-07 18:22:52 +0600

    first commit

---
+++ cal.js
@@ -0,0 +1,20 @@
+const display = document.getElementById('display');
+const ac = document.getElementById('ac');
+const one = document.getElementById('one');
+const two = document.getElementById('two');
+const three = document.getElementById('three');
+const four = document.getElementById('four');
+const five = document.getElementById('five');
+const six = document.getElementById('six');
+const seven = document.getElementById('seven');
+const eight = document.getElementById('eight');
+const nine = document.getElementById('nine');
+togol
+modulas
+div
+mul
+minus
+plus
+fact
+dot
+equal
